d507f4dd6ff486d53035d0eaf898d56ee7c97581
Cesmac <cesmac@academico.cesmac.edu.br>
2020-02-19 18:36:05 -0300

    ff

---
+++ src/main/java/br/edu/cesmac/nolapi/repository/EditoriasRepository.java
@@ -0,0 +1,12 @@
+package br.edu.cesmac.nolapi.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.cesmac.nolapi.resources.domain.Editoria;
+
+@Repository
+public interface EditoriasRepository extends JpaRepository<Editoria, Long> {
+
+}
